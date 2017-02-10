43042cf43f11955f05b2cf01180a036ee7e8a6be
santhisenan <santhisenan@outlook.com>
2017-02-10 17:31:05 +0530

    ui for admin page done

--- ci/application/views/adminView.php
+++ ci/application/views/adminView.php
@@ -1,4 +1,41 @@
-only admins here...welcome...
-<?php 
-echo $_SESSION['username'];
- ?>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Admin</title>
+		<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../css/admin_styles.css">
+		<link rel="stylesheet" href="../css/styles_form.css">
+	</head>
+	<body>
+		<nav class="navbar .navbar-fixed-top naver">
+		<div class="container-fluid">
+			<span>Treasure Hunt Admin</span>
+		</div>
+		<div><a href="adminController">Admin</a></div>
+		</nav>
+
+		<div class="row panel-row">
+		<div class="col-md-2 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-heading"><span class="glyphicon glyphicon-plus"></span></div>
+				<div class="panel-body"><a href="#">add a new team</a></div>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<div class="panel panel-default">
+				<div class="panel-heading"><span class="glyphicon glyphicon-th-list"></span></div>
+				<div class="panel-body"><a href="#">List all teams</a></div>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<div class="panel panel-default">
+				<div class="panel-heading"><span class="glyphicon glyphicon-info-sign"></span></div>
+				<div class="panel-body"><a href="#">View a team</a></div>
+			</div>
+		</div>
+
+		
+		
+		</div>
+	</body>
+</html>
