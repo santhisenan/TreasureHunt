4c49fdb9d38235fb93b14e7f7d85f51586f44c0d
santhisenan <santhisenan@outlook.com>
2017-02-11 16:49:41 +0530

    removed tracking of autoload and config files

--- ci/application/views/createTeamView.php
+++ ci/application/views/createTeamView.php
@@ -3,14 +3,16 @@
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4">
 			<div class="form-container add-new-team-form-container">
-			<span>Add a new team</span>
+			<div class="login-head-container">
+			<div class="login-head">Add a new team</div >
+			</div>
 				<form action="createTeam/addteam" method="POST">
 				<input type="text" class="form-control add-new-team-form" placeholder="Name of the team" name="teamName" required/>
 				<input type="password" class="form-control add-new-team-form" placeholder="Answer" name="answer" required/>
 				<input type="text" class="form-control add-new-team-form" placeholder="Question" name="question" required />
 				<input type="text" class="form-control add-new-team-form" placeholder="Next Level" name="nextLevel" required>
-				<div class="row">
-				<div class="col-md-2 col-md-offset-3">
+				<div class="row form-group">
+				<div class="col-md-4 col-md-offset-4 col-xs-6 col-xs-offset-3 submit-btn-div">
 				<button type="submit" class="btn-primary submit-btn">Submit</button>
 				</div>
 				</div>
